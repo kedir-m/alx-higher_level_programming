16b820313039d43c6292734c0d9133916e3e1b36
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-09-12 14:08:47 +0300

    I used break instead of finally keyword

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        try:
-            for i in range(x):
-                print(my_list[i], end="")
-        except IndexError:
-            return (i)
-        finally:
-            print()
-        return (i + 1)
+        for i in range(x):
+                try:
+                        print(my_list[i], end="")
+                        x = i + 1
+                except IndexError:
+                        x = i
+                        break
+        print()
+        return (x)
