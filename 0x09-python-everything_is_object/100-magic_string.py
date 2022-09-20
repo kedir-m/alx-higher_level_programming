aba6345aa1a0f3b35cd2475f56ba1bd370e9c7bb
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-09-20 23:27:07 +0300

    fixed bugs

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
 def magic_string():
-    return 'Holberton, ' * (magic_string.calls - 1) + 'Holberton'
+    setattr(magic_string, "i", getattr(magic_string, "i", -1) + 1)
+    return "BestSchool" + ", BestSchool" * getattr(magic_string, "i", 0)
