45c5507af7439e93c382e12e7f6a260d2c23bdf2
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-11-04 12:45:37 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of table without name value.
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
